6ed89c4a583ead489d50ba24ef4da0be7b7a620d
Ifycode <marychord@gmail.com>
2021-12-24 04:39:27 +0100

    Completed xylophone app plus upgrade

--- README.md
+++ README.md
@@ -1,20 +1,16 @@
-![App Brewery Banner](https://github.com/londonappbrewery/Images/blob/master/AppBreweryBanner.png)
-
-
 # Xylophone 🎹
 
-## Our Goal
+Xylophone app that plays Xylophone sounds. Extra features added by me include:
+- Note names on xylophone keys
+- Change of background color and text color on key press for interactivity
+- Refresh button for when sound is no more played
 
-In this tutorial we will be diving into more Dart programming concepts and learning how to use open source Flutter packages. By the end of this module, you should be comfortable using Flutter packages and building repeated widgets.
+![xylophone app shot](./assets/xylophone-shot.mov)
 
+# Icon resource
+- [Icon8](https://icons8.com/icons/set/refresh)
 
-## What you will create
-
-We’re going to make a music app that plays Xylophone sounds. For every Beethoven out there, this will let you unleash your musical talent where ever you are. 
-
-![Finished App](https://github.com/londonappbrewery/Images/blob/master/xylophone-flutter.png)
-
-## What you will learn
+## Lessons learnt
 
 - How to incorporate open source libraries of code into your project using Flutter Packages.
 - How to play sound on both iOS and Android.
@@ -22,6 +18,3 @@ We’re going to make a music app that plays Xylophone sounds. For every Beethov
 - How to use Dart functions that can take input arguments as well as return an output.
 - Dart arrow syntax for writing one line functions.
 
->This is a companion project to The App Brewery's Complete Flutter Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
-
-![End Banner](https://github.com/londonappbrewery/Images/blob/master/readme-end-banner.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,182 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+// Upgraded Xylophone (StatefulWidget)
+
+class XylophoneApp extends StatefulWidget {
+  //const XylophoneApp({Key? key}) : super(key: key);
+
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  Color myColor = Colors.black;
+  int testNum = 0;
+
+  void buttonPlaySound(int num) {
+    final player = AudioCache();
+    player.play('note$num.wav');
+  }
+
+  Expanded coloredKey({int keyNum, Color keyColor, String keyNote}) {
+    return Expanded(
+      flex: 4,
+      child: TextButton(
+        onPressed: () {
+          buttonPlaySound(keyNum);
+          setState(() {
+            //1. Make testNum equal to a particular keyNum
+            testNum = keyNum;
+          });
+        },
+        style: ButtonStyle(
+          //2.a. Return color based on if testNum is to a keyNum
+          backgroundColor: MaterialStateProperty.all<Color>(
+              testNum == keyNum ? myColor : keyColor),
+        ),
+        child: Container(
+          height: 40.0,
+          width: 40.0,
+          child: Center(
+            child: Text(
+              '$keyNote',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25.0,
+              ),
+            ),
+          ),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            //2.b. Return color based on if testNum is to a keyNum
+            color: testNum == keyNum ? keyColor : Colors.grey[900],
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                spreadRadius: 3,
+                blurRadius: 10,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              coloredKey(keyNum: 1, keyColor: Colors.red, keyNote: 'd'),
+              coloredKey(keyNum: 2, keyColor: Colors.orange, keyNote: 'r'),
+              coloredKey(keyNum: 3, keyColor: Colors.yellow, keyNote: 'm'),
+              coloredKey(keyNum: 4, keyColor: Colors.green, keyNote: 'f'),
+              coloredKey(keyNum: 5, keyColor: Colors.teal, keyNote: 's'),
+              coloredKey(keyNum: 6, keyColor: Colors.blue, keyNote: 'l'),
+              coloredKey(keyNum: 7, keyColor: Colors.purple, keyNote: 't'),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      //3. Set testNum back to zero (this resets colors)
+                      testNum = 0;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Image.asset('images/refresh.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+/* Original code (StatelessWidget)
+
+
+class XylophoneApp extends StatelessWidget {
+  void buttonPlaySound(int num) {
+    final player = AudioCache();
+    player.play('note$num.wav');
+  }
+
+  Expanded coloredKey({int keyNum, Color keyColor, String keyNote}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          buttonPlaySound(keyNum);
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(keyColor),
+        ),
+        child: Container(
+          height: 40.0,
+          width: 40.0,
+          child: Center(
+            child: Text(
+              '$keyNote',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25.0,
+              ),
+            ),
+          ),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.grey[900],
+            //border: Border.all(color: Colors.black),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                spreadRadius: 3,
+                blurRadius: 10,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              coloredKey(keyNum: 1, keyColor: Colors.red, keyNote: 'd'),
+              coloredKey(keyNum: 2, keyColor: Colors.orange, keyNote: 'r'),
+              coloredKey(keyNum: 3, keyColor: Colors.yellow, keyNote: 'm'),
+              coloredKey(keyNum: 4, keyColor: Colors.green, keyNote: 'f'),
+              coloredKey(keyNum: 5, keyColor: Colors.teal, keyNote: 's'),
+              coloredKey(keyNum: 6, keyColor: Colors.blue, keyNote: 'l'),
+              coloredKey(keyNum: 7, keyColor: Colors.purple, keyNote: 't'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
